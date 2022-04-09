061749ac6e66ec5f3e958adc756896b42173516f
ApolloSolo <matt1teixeira@gmail.com>
2022-04-09 18:29:07 -0400

    user CRUD and login validation with bcrypt

--- models/User.js
+++ models/User.js
@@ -1,7 +1,12 @@
 const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connections");
+const bcrypt = require("bcrypt");
 
-class User extends Model {}
+class User extends Model {
+  checkPassword(loginPass) {
+    return bcrypt.compare(loginPass, this.password);
+  }
+}
 
 User.init(
   {
@@ -32,6 +37,16 @@ User.init(
     },
   },
   {
+    hooks: {
+        async beforeCreate(newUserData) {
+            newUserData.password = await bcrypt.hash(newUserData.password, 10);
+            return newUserData;
+        },
+        async beforeUpdate(updatedUserData) {
+            updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+            return updatedUserData;
+          }
+    },
     sequelize,
     timestamps: false,
     freezeTableName: true,

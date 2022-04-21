40a6547d25214cb0ed8fb1799a4a75cdefc1a170
ApolloSolo <matt1teixeira@gmail.com>
2022-04-21 19:46:19 -0400

    Registration/login/out logic and UI created

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const { User, Wallet } = require("../../models/index");
 const { getUserTickers, calcUserMoney } = require("../../util/alpha-helpers");
+const startingFunds = 10000;
 
 router.get("/", (req, res) => {
   User.findAll({
@@ -37,20 +38,31 @@ router.get("/:id", async (req, res) => {
   res.json(userData);
 });
 
-router.post("/", (req, res) => {
-  User.create({
+//create user
+router.post("/", async (req, res) => {
+  let newUser = await User.create({
     username: req.body.username,
     email: req.body.email,
     password: req.body.password,
-    money: req.body.money,
-  })
-    .then((userData) => res.json(userData))
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+    money: startingFunds,
+  });
+  let newWallet = await Wallet.create({
+    user_id: newUser.id,
+    btc: 0,
+    eth: 0,
+    ltc: 0,
+    atom: 0,
+    doge: 50,
+  });
+  req.session.save(() => {
+    req.session.user_id = newUser.id;
+    req.session.username = newUser.username;
+    req.session.loggedIn = true;
+  });
+  res.json([newUser, newWallet]);
 });
 
+// log in
 router.post("/login", (req, res) => {
   User.findOne({
     where: {
@@ -66,8 +78,26 @@ router.post("/login", (req, res) => {
       res.status(400).json({ message: "Incorrect password!" });
       return;
     }
-    res.json({ user: userData, message: "You have logged in!" });
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.username = userData.username;
+      req.session.loggedIn = true;
+
+      res.json({ user: userData, message: "You are now logged in!" });
+    });
   });
+});
+
+router.post('/logout', (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  }
+  else {
+    res.status(404).end();
+  }
 });
 
 router.put("/:id", (req, res) => {
